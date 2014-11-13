cac980a9276d346efa05ea04fc64efb15b6329a5
William Wnekowicz <william.wnekowicz@gmail.com>
2014-11-13 12:29:33 -0800

    Adds version tag to top-level registry to fix #2

--- start/start-registry.sh
+++ start/start-registry.sh
@@ -1,1 +1,1 @@
-docker run -d --name registry -v ~/images:/tmp/registry -p 127.0.0.1:5000:5000 -e SETTINGS_FLAVOR=local registry
+docker run -d --name registry -v ~/images:/tmp/registry -p 127.0.0.1:5000:5000 -e SETTINGS_FLAVOR=local registry:0.9.0

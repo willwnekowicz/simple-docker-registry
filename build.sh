5eab8e8ebfb414e3f4bb639f671b693ef85705c0
William Wnekowicz <william.wnekowicz@gmail.com>
2014-11-13 12:21:07 -0800

    Uses top-level docker registry image to close #2

--- build.sh
+++ build.sh
@@ -29,11 +29,7 @@ if ! command -v htpasswd > /dev/null; then
 fi
 
 #Get Docker Registry
-git clone https://github.com/docker/docker-registry.git && cd docker-registry
-git checkout 0.9.0 && cd ..
-
-#Build Docker Registry container
-docker build -t private-registry docker-registry/.
+docker pull registry:0.9.0
 
 #Run Docker Registry container
 ./start/start-registry.sh
--- start/start-registry.sh
+++ start/start-registry.sh
@@ -1,1 +1,1 @@
-docker run -d --name registry -v ~/images:/tmp/registry -p 127.0.0.1:5000:5000 -e SETTINGS_FLAVOR=local private-registry
+docker run -d --name registry -v ~/images:/tmp/registry -p 127.0.0.1:5000:5000 -e SETTINGS_FLAVOR=local registry
